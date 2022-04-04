9da3dc9f3fff7ef4251162c2aff965f0828f669b
Nosdezin <72828159+nosdezin@users.noreply.github.com>
2022-04-04 10:36:51 -0300

    fiz

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const MatrixHunterWord = [
-  ["b", "o", "a", "f"],
-  ["g", "h", "i", "a"],
-  ["j", "k", "l", "o"],
+  ["b", "a", "a", "f"],
+  ["o", "h", "i", "a"],
+  ["a", "k", "l", "o"],
   ["m", "n", "a", "b"],
 ];
 
 const gabarito = "boa";
-let alvo = {
+const alvo = {
   x: 0,
   y: 0,
 };
@@ -38,95 +38,108 @@ const ULTIMA_LETRA =
   MatrixHunterWord[MatrixHunterWord.length - 1][COLUNA_TAMANHO - 1];
 
 const GET_ANSWER = (tipo, resposta, linha, coluna) => {
+  let answer = "";
+  // console.log(tipo, "|resposta: " + resposta + "|", linha, coluna);
   switch (tipo) {
     case "cima":
-      resposta = gabarito[0];
+      answer = gabarito[0];
       for (let index = circle.top.x; index >= 0; index--) {
-        if (resposta != gabarito) {
-          resposta += MatrixHunterWord[index][circle.top.y];
+        if (answer != gabarito) {
+          answer += MatrixHunterWord[index][circle.top.y];
         }
       }
       break;
     case "esquerda":
       console.log("letra encontrada na direita");
-      resposta = gabarito[0];
+      answer = gabarito[0];
       for (let index = circle.left.y; index >= 0; index--) {
-        if (resposta != gabarito) {
-          resposta += MatrixHunterWord[circle.left.x][index];
+        if (answer != gabarito) {
+          answer += MatrixHunterWord[circle.left.x][index];
         }
       }
       break;
     case "direita":
-      resposta = gabarito[0];
+      answer = gabarito[0];
       for (let index = circle.right.y; index < COLUNA_TAMANHO; index++) {
-        if (resposta != gabarito) {
-          resposta += MatrixHunterWord[circle.right.x][index];
+        if (answer != gabarito) {
+          answer += MatrixHunterWord[circle.right.x][index];
         }
       }
       break;
     case "baixo":
-      resposta = gabarito[0];
+      answer = gabarito[0];
       for (let index = circle.bottom.x; index < COLUNA_TAMANHO; index++) {
-        if (resposta != gabarito) {
-          resposta += MatrixHunterWord[index][circle.bottom.y];
+        if (answer != gabarito) {
+          answer += MatrixHunterWord[index][circle.bottom.y];
         }
       }
       break;
     case "baixo-direito":
-      resposta = gabarito[0];
+      answer = gabarito[0];
       linha = circle.bottomRight.x;
       coluna = circle.bottomRight.y;
       while (linha <= 3 && coluna <= 3) {
-        if (resposta != gabarito) {
-          resposta += MatrixHunterWord[linha][coluna];
+        if (answer != gabarito) {
+          answer += MatrixHunterWord[linha][coluna];
         }
         linha++;
         coluna++;
       }
       break;
     case "baixo-esquerdo":
-      resposta = gabarito[0];
+      answer = gabarito[0];
       linha = circle.bottomLeft.x;
       coluna = circle.bottomLeft.y;
       while (linha >= 0 && coluna >= 0) {
-        if (resposta != gabarito) {
-          resposta += MatrixHunterWord[linha][coluna];
+        if (answer != gabarito) {
+          answer += MatrixHunterWord[linha][coluna];
         }
         linha++;
         coluna--;
       }
       break;
     case "cima-esquerda":
-      resposta = gabarito[0];
+      answer = gabarito[0];
       linha = circle.topLeft.x;
       coluna = circle.topLeft.y;
       while (linha >= 0 && coluna >= 0) {
-        if (resposta != gabarito) {
-          resposta += MatrixHunterWord[linha][coluna];
+        if (answer != gabarito) {
+          answer += MatrixHunterWord[linha][coluna];
         }
         linha--;
         coluna--;
       }
       break;
     case "cima-direito":
-      resposta = gabarito[0];
+      answer = gabarito[0];
       linha = circle.topRight.x;
       coluna = circle.topRight.y;
       while (linha >= 0 && coluna <= 3) {
-        if (resposta != gabarito) {
-          resposta += MatrixHunterWord[linha][coluna];
+        if (answer != gabarito) {
+          answer += MatrixHunterWord[linha][coluna];
         }
         linha--;
         coluna++;
       }
       break;
+    default:
+      console.log("Deu errado");
+      break;
   }
+  if (answer != gabarito) {
+    console.log("RESPOSTA NAO CORRESPONDE AO GABARITO");
+  }
+
+  resposta = answer;
+  // console.log(tipo, "|resposta: " + resposta + "|", linha, coluna);
+  return resposta;
 };
 
 let linha = 0;
 let coluna = 0;
 
 const HUNTER_WORD_FUNCTION = () => {
+  console.log("Iniciando o bot");
   if (MatrixHunterWord[alvo.x][alvo.y] == gabarito[0]) {
     if (circle.top.x != -1 || circle.top.x > MatrixHunterWord.length) {
       GET_ANSWER("cima", resposta, linha, coluna);
@@ -136,7 +149,7 @@ const HUNTER_WORD_FUNCTION = () => {
           MatrixHunterWord[circle.left.x][circle.left.y] ==
           SEGUNDA_LETRA_GABARITO
         ) {
-          GET_ANSWER("esquerda", resposta.linha, coluna);
+          GET_ANSWER("esquerda", resposta, linha, coluna);
         }
       } else {
         if (circle.right.y != -1 || circle.right.y > MatrixHunterWord.length) {
@@ -144,7 +157,7 @@ const HUNTER_WORD_FUNCTION = () => {
             MatrixHunterWord[circle.right.x][circle.right.y] ==
             SEGUNDA_LETRA_GABARITO
           ) {
-            GET_ANSWER("direita", resposta.linha, coluna);
+            resposta = GET_ANSWER("direita", resposta, linha, coluna);
           }
         } else {
           if (circle.bottom.x != -1 || circle.bottom.x != 4) {
@@ -152,7 +165,7 @@ const HUNTER_WORD_FUNCTION = () => {
               MatrixHunterWord[circle.bottom.x][circle.bottom.y] ==
               SEGUNDA_LETRA_GABARITO
             ) {
-              GET_ANSWER("baixo", resposta.linha, coluna);
+              GET_ANSWER("baixo", resposta, linha, coluna);
             }
           } else {
             if (
@@ -206,25 +219,27 @@ const HUNTER_WORD_FUNCTION = () => {
         }
       }
     }
-  }else{
-    console.log("letra não encontrada");
+  } else {
+    console.log("letra nao encontrada");
   }
+  console.log("Terminando  o bot");
 };
 
 console.log(MatrixHunterWord);
+HUNTER_WORD_FUNCTION();
 
-while (ciclo_completo == false) {
-  HUNTER_WORD_FUNCTION();
-  console.log("AlvoX: ",alvo.x," AlvoY: ",alvo.y);
-  if (alvo.x == MatrixHunterWord.length - 1 && alvo.y == COLUNA_TAMANHO - 1) {
-    ciclo_completo = true;
-  } else {
-    if (alvo.x == 3) {
-      alvo.x = 0;
-      alvo.y += 1;
-    }
-  }
-  alvo.x += 1;
-}
+// while (ciclo_completo == false) {
+//   HUNTER_WORD_FUNCTION();
+//   console.log("AlvoX: ",alvo.x," AlvoY: ",alvo.y);
+//   if (alvo.x == MatrixHunterWord.length - 1 && alvo.y == COLUNA_TAMANHO - 1) {
+//     ciclo_completo = true;
+//   } else {
+//     if (alvo.x == 3) {
+//       alvo.x = 0;
+//       alvo.y += 1;
+//     }
+//   }
+//   alvo.x += 1;
+// }
 
 console.log("Resposta: ", resposta);
